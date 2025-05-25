1f59fa28547572e13d5b1e1d76c7aee671f4d77e
ThakurSaad <thakursaad613@gmail.com>
2025-05-25 15:20:27 +0600

    switch case cpp

---
+++ module-1/switch_case.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    switch (x % 2)
+    {
+    case 0:
+        cout << "EVEN";
+        break;
+    case 1:
+        cout << "ODD";
+        break;
+    default:
+        cout << "TF??";
+    }
+
+    return 0;
+}
