abee68a448607d5d09a75b391c2eee8905baa73f
ThakurSaad <thakursaad613@gmail.com>
2025-05-28 21:45:59 +0600

    sort in c++

---
+++ module-3/sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + n); // ascending
+    // sort(a, a + n, greater<int>()); // descending
+    // sort(a, a + 4);
+    // sort(a + 2, a + 5);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
