1e7230b0793d395963ccc2341c381025d719f4b8
ThakurSaad <thakursaad613@gmail.com>
2025-05-29 19:02:53 +0600

    mathematical exprssn

---
+++ module-3.5/mathmetical_expression.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    char s;
+    int b;
+    char q;
+    int c;
+
+    cin >> a >> s >> b >> q >> c;
+
+    return 0;
+}
