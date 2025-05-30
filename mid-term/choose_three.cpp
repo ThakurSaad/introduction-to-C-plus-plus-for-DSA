8df3bf42292a5ea06bcf3aa2cdfcfcd1d42c597e
ThakurSaad <thakursaad613@gmail.com>
2025-05-30 18:47:12 +0600

    choose three

---
+++ mid-term/choose_three.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+
+        int n, s;
+        cin >> n >> s;
+
+        int a[n];
+
+        for (int m = 0; m < n; m++)
+        {
+            cin >> a[m];
+        }
+
+        int is_sum = 0;
+
+        for (int j = 0; j < n - 2; j++)
+        {
+
+            if ((n == 1) || (n == 2))
+            {
+                is_sum = 0;
+                break;
+            }
+
+            for (int k = j + 1; k < n - 1; k++)
+            {
+
+                for (int l = k + 1; l < n; l++)
+                {
+                    int sum = a[j] + a[k] + a[l];
+                    if (sum == s)
+                    {
+                        is_sum = 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (is_sum)
+        {
+            // cout << is_sum << " = YES" << endl;
+            cout << "YES" << endl;
+        }
+        else
+        {
+            // cout << is_sum << " = NO" << endl;
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
