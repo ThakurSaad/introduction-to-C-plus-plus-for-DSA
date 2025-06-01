2614b6dd21438a660de63f87788d38fecb62fa40
ThakurSaad <thakursaad613@gmail.com>
2025-06-01 19:55:17 +0600

    string modifier functions

---
+++ module-5/string_modifiers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "hello diego. wby?";
+
+    // s.replace(9, 0, "m");
+    s.insert(5, " hello");
+
+    cout << s;
+
+    return 0;
+}
