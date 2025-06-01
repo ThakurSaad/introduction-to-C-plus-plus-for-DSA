df23474e8dde79a9259c3f56075b69070b901f58
ThakurSaad <thakursaad613@gmail.com>
2025-06-01 19:12:03 +0600

    string capacity functions

---
+++ module-5/string_capacity_fn.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s1 = "myasdfa";
+    string s;
+    // s1.resize(6, 98);
+
+    cout << s.capacity();
+
+    return 0;
+}
