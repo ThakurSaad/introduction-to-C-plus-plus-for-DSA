4f74e9d773654cb28bb4c42c6b107b3b496b055d
ThakurSaad <thakursaad613@gmail.com>
2025-06-01 18:32:16 +0600

    string capacity

---
+++ module-5/string.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    s = "hi";
+    cout << s << endl;
+
+    s = "no";
+    cout << s << endl;
+
+    s = "no never not ever";
+    cout << s << endl;
+
+    return 0;
+}
