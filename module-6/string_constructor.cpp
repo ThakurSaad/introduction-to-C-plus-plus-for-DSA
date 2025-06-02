f5f6ff2f56bbcf60c814dee36b928a167b9d1966
ThakurSaad <thakursaad613@gmail.com>
2025-06-02 10:09:33 +0600

    string constructor

---
+++ module-6/string_constructor.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "hello";
+
+    // string s("hello");
+    // string s("hello", 4);
+
+    // string s = "hello world";
+    // string t(s, 5);
+    string s(5, 'e');
+
+    cout << s;
+
+    return 0;
+}
