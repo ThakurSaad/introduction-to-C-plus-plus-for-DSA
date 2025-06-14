dcbe556e817c363f9756bb7e23013d4e7e2798a7
ThakurSaad <thakursaad613@gmail.com>
2025-06-14 18:29:09 +0600

    replace word

--- final/replace.cpp
+++ final/replace.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        string s, t;
+        cin >> s >> t;
+
+        int t_ln = t.size();
+
+        for (int j = 0; j <= 1000; j++)
+        {
+            if (s.find(t) > 1001)
+            {
+                break;
+            }
+            s.replace(s.find(t), t_ln, "#");
+        }
+        cout << s << endl;
+    }
 
     return 0;
 }
